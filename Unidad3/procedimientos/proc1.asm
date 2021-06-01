bb18d45f45ed7f72833a58c4e565999ebb73e045
Jacob Green <jacob@ellipz.io>
2021-06-01 10:05:16 -0600

    Ejemplo procedimiento.

---
+++ Unidad3/procedimientos/proc1.asm
@@ -0,0 +1,35 @@
+section .data
+    varX dd 5
+    varY dd 6
+section .bss
+
+global main
+
+section .text
+main:
+
+    mov rax,[varX]
+    mov rbx,[varY]
+
+    sub rsp,8 ; reservamos espacio en la pila para el valor de retorno
+    push rax ; pasamos primer parametro 'x'
+    push rbx ; pasamos segundo parametro 'y'
+    call miFuncion
+    ; resultado se encuentra en el tope de la pila
+    pop rdi
+    jmp salir
+
+miFuncion:
+    mov rcx,[rsp+16] ; recuperamos 'x'
+    mov rdx,[rsp+8] ; recuperamos 'y'
+    imul rcx,rdx ; rcx = rcx * rdx ; x = x * y
+    mov [rsp+24],rcx ; colocamos el resultado en la posicion reservada
+    mov rdi,[rsp] ; guardamos rsp en rdi
+    mov [rsp+16],rdi ; movemos la direccion de retorno a rsp+16
+    add rsp,16
+    ret ; regresar a la posicion donde estaba al momento de llamar a miFuncion
+
+salir:
+    mov rax,60
+    mov rdi,0
+    syscall
