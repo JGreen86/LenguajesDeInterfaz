0ec2f8f3a2bc53899c1b94c6188b9f14bb6fa776
Jacob Green <jacob@ellipz.io>
2022-03-09 20:13:55 -0700

    Ejemplo de operaciones logica.

--- Unidad2/operacionesLogicas.asm
+++ Unidad2/operacionesLogicas.asm
@@ -1,16 +1,41 @@
 section .data
     op1 db 0b01000001
     op2 db 0b01100010
+
+    op3 db "A"
+    op4 db "j"
 section .bss
     resul resb 1
 
 global main
 section .text
 main:
-    mov rax,[op1]
-    AND rax,[op2]
-    mov [resul],rax
+    mov al,[op1]
+    AND al,[op2]
+    mov [resul],al
+;   jmp imprimir
 
+    mov al,[op3]
+    AND al,[op4]
+    mov [resul],al
+;   jmp imprimir
+
+    mov al,[op3]
+    OR al,[op4]
+    mov [resul],al
+;   jmp imprimir
+
+    mov al,[op3]
+    XOR al,[op4]
+    XOR al,[op4]
+    mov [resul],al
+;    jmp imprimir
+
+    mov al,[op3]
+    NOT al
+    mov [resul],al
+;   jmp imprimir
+imprimir:
     ; sys_write
     mov rax,1
     mov rdi,1
